ae08479f6ee1456a1d003132d01079731aa37dc8
Moti Abe <motiabe8@gmail.com>
2026-06-09 08:50:53 +0300

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += s + "🩷"
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        return s.split("🩷")[:-1]
+        
+
+        
+
